7b43221ec7d0e478ccc1d099cb8bc47ad08b4f18
nstaso1 <117488392+nstaso1@users.noreply.github.com>
2023-11-25 20:40:49 -0800

    Create Account.java

---
+++ Assignment6/Account.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package assignment6_1;
+
+/**
+ *
+ * @author natha
+ */
+import java.io.Serializable;
+
+public class Account implements Serializable
+{
+    protected String name; // The person who owns the account
+    protected double balance;// do not define this in CheckingAccount class
+    
+    public Account(String acctName, double initBalance)
+    {
+    balance = initBalance;
+    name = acctName;
+    }
+    public String getName() 
+    {
+    return name;
+    }
+    public void setName(String name) 
+    {
+    this.name = name;
+    }
+    public double getBalance() 
+    {
+    return balance;
+    }
+    public void setBalance(double balance) 
+    {
+    this.balance = balance;
+    }
+}
