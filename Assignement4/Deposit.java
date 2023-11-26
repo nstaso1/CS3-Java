fccb7ccc788942736746f759339458b9cc27ec55
nstaso1 <117488392+nstaso1@users.noreply.github.com>
2023-11-25 20:31:33 -0800

    Create Deposit.java

---
+++ Assignement4/Deposit.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package assignment4;
+
+/**
+ *
+ * @author natha
+ */
+public class Deposit extends Transaction
+{
+private int depositNumber; //  number for each deposit transaction
+public Deposit(int tCount, int tId, double tAmt, int checkNum, int depositCheck, int depositCash) {
+super(tCount, tId, tAmt, checkNum, depositCheck, depositCash);
+this.depositNumber = depositNumber;
+}
+public int getDepositNumber() {
+return depositNumber;
+}
+public void setDepositNumber(int DepositNumber) {
+this.depositNumber = depositNumber;
+}
+}
+
