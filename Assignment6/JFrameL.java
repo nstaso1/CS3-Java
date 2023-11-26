96808e98769bd9ddd05347c5a07622c227119964
nstaso1 <117488392+nstaso1@users.noreply.github.com>
2023-11-25 20:39:10 -0800

    Create JFrameL.java

---
+++ Assignment6/JFrameL.java
@@ -0,0 +1,31 @@
+package assignment6_1;
+
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+public class JFrameL extends JFrame
+{
+    /** Creates a new instance of JFrameL */
+    public JFrameL(String title) {
+        super(title);
+        FrameListener listener = new FrameListener();
+        addWindowListener(listener);
+    }
+   private class FrameListener extends WindowAdapter
+   {
+
+    public void windowClosing(WindowEvent e) {
+        int confirm;
+        if (!Main.saved)
+        {
+           String  message = "The data in the application is not saved.\n"+
+               "Would you like to save it before exiting the application?";
+           confirm = JOptionPane.showConfirmDialog (null, message);
+           if (confirm == JOptionPane.YES_OPTION)
+              Main.chooseFile(2); 
+        }
+       System.exit(0);
+    }
+   }   
+}
